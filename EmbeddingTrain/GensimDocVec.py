3c7ac141103584437b429f48410a13718cb598cf
chenyanxiong <xiongchenyan@gmail.com>
2015-04-07 11:38:29 -0400

    training api finished

--- EmbeddingTrain/GensimDocVec.py
+++ EmbeddingTrain/GensimDocVec.py
@@ -51,7 +51,7 @@ from gensim.models.doc2vec  import *
 
 import logging
 
-class GensimDocVecTrainC(cxBaseC):
+class GensimDocVecC(cxBaseC):
     
     def Init(self):
         cxBaseC.Init(self)
@@ -78,6 +78,60 @@ class GensimDocVecTrainC(cxBaseC):
         self.dm = int(self.conf.GetConf('dm', self.dm))
         self.InName = self.conf.GetConf('in')
         self.OutName = self.conf.GetConf('out')
+    
+    
+    @staticmethod
+    def ShowConf():
+        cxBaseC.ShowConf()    
+        print "dm defines the training algorithm. By default (dm=1), distributed memory is used. Otherwise, dbow is employed."
+        print "size is the dimensionality of the feature vectors."
+        print "window is the maximum distance between the current and predicted word within a sentence."
+        print "alpha is the initial learning rate (will linearly drop to zero as training progresses)."
+        print "seed = for the random number generator."
+        print "min_count = ignore all words with total frequency lower than this."
+        print "sample = threshold for configuring which higher-frequency words are randomly downsampled;"
+        print "default is 0 (off), useful value is 1e-5."
+        print "workers = use this many worker threads to train the model (=faster training with multicore machines)."
+        print "hs = if 1 (default), hierarchical sampling will be used for model training (else set to 0)."
+        print "negative = if > 0, negative sampling will be used, the int for negative specifies how many “noise words” should be drawn (usually between 5-20)."
+        print "dm_mean = if 0 (default), use the sum of the context word vectors. If 1, use the mean. Only applies when dm is used."
+        print "in\nout"
+    
+    def Train(self):
+        sentence = LineSentence(self.InName)
+        logging.info('start training on [%s]',self.InName)
+        model = DocVec(sentence,size=self.size,min_count=self.min_count,sample=self.sample,workers=self.workers,dm=self.dm,hs=self.hs,negative=self.negative)
+        
+        logging.info('training complete')
+        model.save(self.OutName)
+        logging.info('saved to [%s]',self.OutName)
+        return True
+    
+    
+    def Process(self):
+        self.Train()
+        
+        
+if __name__=='__main__':
+    import sys
+    if 2 != len(sys.argv):
+        GensimDocVecC.ShowConf()
+        sys.exit()
+        
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    
+    
+    module = GensimDocVecC(sys.argv[1])
+    module.Process()
+    
+    logging.info('complete')
         
         
         

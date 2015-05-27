37dc7956cb1c7f58d8ed08d43f0699343a3956e7
xiongchenyan <xiongchenyan@gmail.com>
2015-05-27 16:05:33 +0800

    kde lm implemented
    basic function implemented
    bandwidth selected via CV

---
+++ ContinuousLanguageModel/KernelDensityLm.py
@@ -0,0 +1,51 @@
+'''
+Created on my MAC May 27, 2015-3:51:44 PM
+What I do:
+I estimate kernel density for each document as its Language model
+    in the continuous space
+What's my input:
+lTerms
+What's my output:
+a kde, can sample, can calculate pdf of a given term
+@author: chenyanxiong
+'''
+
+
+
+import numpy as np
+
+from sklearn.neighbors import KernelDensity
+from sklearn.grid_search import GridSearchCV
+import logging
+
+class KernelDensityLmC(object):
+    def __init__(self,lTerm = [],Word2VecModel = None):
+        self.Init()
+        if ([] != lTerm) & (None != Word2VecModel):
+            self.Construct(lTerm,Word2VecModel)
+            
+            
+    def Init(self):
+        self.kde = KernelDensity()
+        self.lBandWidth=np.logspace(-10, 10, 20)
+        self.lX = []
+        
+    def Construct(self,lTerm,Word2VecModel):
+        self.lX = [Word2VecModel[term] for term in lTerm if term in Word2VecModel]
+        
+        self.kde = self.CVForBestKde()
+        
+        
+    def CVForBestKde(self):
+        params = {'bandwidth':self.lBandWidth}
+        grid = GridSearchCV(KernelDensity(), params)
+        grid.fit(self.lX)
+        logging.info('best bandwidth = [%f]',grid.best_estimator_.bandwidth)
+        return grid.best_estimator_
+    
+    
+    def pdf(self,x):
+        return self.kde.score(x)
+        
+
+

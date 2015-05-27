da1c1c02d3b05b0d247a2ac646b219659d3f39ec
xiongchenyan <xiongchenyan@gmail.com>
2015-05-27 16:05:17 +0800

    init corpus gaussian lm train

---
+++ ContinuousLanguageModel/CorpusGaussianLmTrain.py
@@ -0,0 +1,18 @@
+'''
+Created on my MAC May 26, 2015-5:50:23 PM
+What I do:
+Train the Gaussian Lm for the entire corpus
+
+What's my input:
+the term ctf pre calculated
+the entire word2vec model
+What's my output:
+mean
+sigma
+precision matrix
+@author: chenyanxiong
+'''
+
+'''
+I don't think it will work at all
+'''

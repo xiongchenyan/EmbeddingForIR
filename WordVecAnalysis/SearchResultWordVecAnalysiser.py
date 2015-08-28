2065b3143cbd13c8d9fe0c345e3d382674c0b97c
chenyanxiong <xiongchenyan@gmail.com>
2015-08-28 14:26:39 -0400

    add correlation test

--- WordVecAnalysis/SearchResultWordVecAnalysiser.py
+++ WordVecAnalysis/SearchResultWordVecAnalysiser.py
@@ -24,32 +24,32 @@ from cxBase.Conf import cxConfC
 from cxBase.base import cxBaseC
 from IndriSearch.IndriSearchCenter import IndriSearchCenterC
 from IndriSearch.IndriDocBase import IndriDocBaseC
-from MarginalDistAnalysiser import MarginalDistAnalysiserC
 import gensim
 import numpy as np
-
+import pickle
+from scipy.stats import pearsonr
 
 class SearchResultWordVecAnalysiserC(cxBaseC):
     
     def Init(self):
         cxBaseC.Init(self)
-        self.MarginalAna = MarginalDistAnalysiserC()
         self.QIn = ""
         self.OutDir = ""
         self.Word2VecInName = ""
         self.Word2VecModel = None
         self.Searcher = IndriSearchCenterC()
+        self.BinNumber = 100
         
     def SetConf(self, ConfIn):
         cxBaseC.SetConf(self, ConfIn)
         
-        self.MarginalAna.SetConf(ConfIn)
         self.Searcher.SetConf(ConfIn)
         
         self.Word2VecInName = self.conf.GetConf('word2vecin')
         self.LoadWord2Vec()
         self.QIn = self.conf.GetConf('in')
         self.OutDir = self.conf.GetConf('outdir')
+        self.BinNumber = self.conf.GetConf('binnumber', self.BinNumber)
         
     def LoadWord2Vec(self):
         logging.info('start load word2vec input')
@@ -58,9 +58,8 @@ class SearchResultWordVecAnalysiserC(cxBaseC):
     @staticmethod
     def ShowConf():
         cxBaseC.ShowConf()
-        print 'word2vecin\nin\noutdir'
+        print 'word2vecin\nin\noutdir\nbinnumber'
         IndriSearchCenterC.ShowConf()
-        MarginalDistAnalysiserC.ShowConf()
         
         
     def LoadDocWordVec(self):
@@ -77,19 +76,78 @@ class SearchResultWordVecAnalysiserC(cxBaseC):
             
         lX = [self.Word2VecModel[term] for term in lTerm if term in self.Word2VecModel]
         
-        logging.info('add doc word vec get')
+        logging.info('target doc word vec get')
         return lX
     
+    
+    def BinData(self,lX,OutName):
+        '''
+        bin all lX's dim
+        [[mu,sigma, bins]]
+        '''
+        logging.info('binning data')
+        lBinData = []
+        for i,x in enumerate(lX):
+            logging.info('binning dim [%d]',i)
+            mu = np.mean(x)
+            sigma = np.var(x)
+            hist,bins = np.histogram(x,bins=self.BinNumber)
+            lBinData.append([mu,sigma,hist, bins])
+        
+        out = open(OutName,'w')
+        
+        pickle.dump(lBinData,out)
+        out.close()
+        logging.info('data binned to [%s]',OutName)
+        return
+    
+    def CalcPersonCorrelation(self,lX,OutName):
+        
+        n,d = lX.shape
+        mPValue = np.zeros([d,d])
+        mPearson = np.zeros([d,d])
+        
+        for i in range(d):
+            for j in range(i+1,d):
+                per,p = pearsonr(lX[:,i],lX[:,j])
+                mPValue[i,j] = p
+                mPValue[j,i] = p
+                mPearson[i,j] = per
+                mPearson[j,i] = per
+                if p < 0.05:
+                    logging.info('[%d-%d] correlated p=%f',i,j,p)
+        
+        out = open(OutName + '_pearson','w')
+        print >>out, np.array2string(mPearson)
+        out.close()
+        
+        out = open(OutName + '_pvalue','w')
+        print >>out, np.array2string(mPValue)
+        out.close()
+        
+        logging.info('pearson corr calculated and dumped')
+        
+        return True
+        
+        
     
     def Process(self):
         
         lX = self.LoadDocWordVec()
     
         self.MarginalAna.BinData(lX, self.OutDir + '/MarginalDist')
+    
+        self.CalcPersonCorrelation(lX,self.OutDir + '/PersonCorrelationMtx')
         
         logging.info('[%s] search result word vec analysis finished',self.QIn)
+    
+    
         
         return True
+    
+    
+    
+    
     
     
 if __name__=='__main__':
